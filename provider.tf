a6ad8979ce2699784218c1e04e6f6ef19339d65a
harikesh12 <96760177+harikesh12@users.noreply.github.com>
2022-10-06 12:06:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIATV7NU5UI2Q4WHDPU"
-  secret_key = "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"
+  access_key = " "
+  secret_key = " "
 
 }
